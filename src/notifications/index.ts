6c7bb40f826d459bf9ff999952eaf096422ecaa2
Salvador Fandiño <sfandino@yahoo.com>
2026-05-23 07:13:16 +0000

    [tsk-t1-prune-notifications-index] [t1-prune-notifications-index] prune notifications index surface

--- src/notifications/index.ts
+++ src/notifications/index.ts
@@ -1,3 +1,16 @@
-export * from './notification-center.js';
-export * from './notification-delivery.js';
-export * from './notification-triggers.js';
+export { NotificationCenter } from './notification-center.js';
+export type { NotificationInput, NotificationOwnership } from './notification-center.js';
+export {
+  NotificationDeliveryService,
+  clearProjectNotificationDeliveryAdapters,
+  createNotificationDeliveryService,
+  setProjectNotificationDeliveryAdapters,
+} from './notification-delivery.js';
+export type { NotificationDeliveryAdapter, NotificationDeliveryContext, NotificationDeliveryTarget } from './notification-delivery.js';
+export {
+  enqueueCardMutationNotifications,
+  enqueueNoteNotifications,
+  enqueueProcessReconciliationNotification,
+  enqueueRuntimeStateNotifications,
+} from './notification-triggers.js';
+export type { NotificationSourceMeta } from './notification-triggers.js';
