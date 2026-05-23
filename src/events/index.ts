ed0f08b5e3deb9d61e17dbf6d9b4a9489a90f494
Salvador Fandiño <sfandino@yahoo.com>
2026-05-23 07:49:51 +0000

    [tsk-t1-preflight-and-prune-events-index] [t1-preflight-and-prune-events-index] prune events index exports

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,2 +1,29 @@
-export * from './registry.js';
-export * from './bus.js';
+export {
+  EventRegistry,
+  eventKindValues,
+  runtimeEventKindValues,
+  agentEventKindValues,
+  trackedEventKindValues,
+  broadcastEventKindValues,
+  operatorBroadcastEventKindValues,
+  isOperatorBroadcastEventKind,
+  getEventSeverity,
+  buildLoggedEventSchema,
+  type SeverityLevel,
+  type OutboundPolicy,
+  type EventKind,
+  type EventPayload,
+  type EventSeverity,
+  type OperatorBroadcastEventKind,
+} from './registry.js';
+
+export {
+  SEVERITY_ORDER,
+  EventBus,
+  BusDisposed,
+  toLoggedEvent,
+  type DomainEvent,
+  type Subscription,
+  type EventHandler,
+  type SubscriptionOptions,
+} from './bus.js';
