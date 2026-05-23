8d8a5d1580e6b007e4b1c9181d0938c5e74d4742
Salvador Fandiño <sfandino@yahoo.com>
2026-05-23 07:04:13 +0000

    [tsk-t1-prune-cards-index] [t1-prune-cards-index] prune cards package index

--- src/cards/index.ts
+++ src/cards/index.ts
@@ -1,4 +1,29 @@
-export * from './card-store.js';
-export * from './artifacts.js';
-export * from './notes.js';
-export * from './diary.js';
+export {
+  CardStore,
+} from './card-store.js';
+export type {
+  CardDiffEntry,
+  CardMutationContext,
+  CardStoreCanonicalHealth,
+  CardStoreHealth,
+} from './card-store.js';
+
+export {
+  registerArtifact,
+  registerAttachment,
+} from './artifacts.js';
+
+export {
+  appendNote,
+  deleteAllNotes,
+  deleteNote,
+  findUnhandledNoteCardId,
+  getNotes,
+  getReconciledUnhandledNotesQueue,
+  markNoteHandled,
+} from './notes.js';
+
+export {
+  deleteDiary,
+  getDiaryEntries,
+} from './diary.js';
